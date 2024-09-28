672cd16d6a966af878ea389eac7b0ec9999fa6f3
Adrien Audebert <adrien.adbrt@tutanota.com>
2024-09-28 20:30:18 +0200

    update grep

--- after/plugin/telescope.lua
+++ after/plugin/telescope.lua
@@ -1,10 +1,37 @@
 local builtin = require('telescope.builtin')
+local actions = require('telescope.actions')
+local actions_state = require('telescope.actions.state')
 
-vim.keymap.set('n', '<C-p>', builtin.git_files, {})
-vim.keymap.set('n', '<leader>pf', builtin.find_files, {})
-vim.keymap.set('n', '<leader>ps', function()
-	builtin.grep_string({ search = vim.fn.input("Grep > ") });
+require('telescope').setup{
+  defaults = {
+    -- Layout and display options for better usability
+    layout_config = {
+      prompt_position = "top",
+    },
+    sorting_strategy = "ascending",
+    file_ignore_patterns = { "node_modules", ".git/" },  -- Ignore certain directories
+    mappings = {
+      i = {
+        ["<C-j>"] = actions.move_selection_next,  -- Move down in the result list
+        ["<C-k>"] = actions.move_selection_previous,  -- Move up in the result list
+		},
+    },
+  },
+  pickers = {
+    -- Customize pickers for `find_files` or `live_grep` here if necessary
+    find_files = {
+      hidden = true,  -- Include hidden files in search
+    },
+    live_grep = {
+      additional_args = function() return { "--hidden" } end  -- Grep in hidden files
+    },
+  },
+  extensions = {
+    -- Any extensions can be configured here
+  },
+}
 
-end)
-vim.keymap.set('n', '<leader>pg', builtin.live_grep, {})
-
+-- Key mappings
+vim.keymap.set('n', '<C-p>', builtin.git_files, {})  -- Search Git-tracked files
+vim.keymap.set('n', '<leader>pf', builtin.find_files, {})  -- Search all files (hidden files included)
+vim.keymap.set('n', '<leader>pg', builtin.live_grep, {})  -- Search text in project (live grep)
